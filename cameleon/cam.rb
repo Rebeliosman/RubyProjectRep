6381db92f95e20d11da260fc3bcf30acee7bc51f
Rebel <rebeliosman@gmail.com>
2020-04-20 16:29:45 +0300

    class lessons

---
+++ cameleon/cam.rb
@@ -0,0 +1,5 @@
+require_relative 'class'
+
+cameleon = Cam.new
+cameleon.change_color ("red")
+cameleon.change_color ("fuck")
---
+++ cameleon/class.rb
@@ -0,0 +1,8 @@
+class Cam
+  def initialize
+  color_name = "native"
+  def change_color (color_name)
+  puts "Cameleon is #{color_name}"
+end
+end
+end
