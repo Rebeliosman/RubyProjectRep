e82c1619fc64df95b953ff5572fda98fa591adbf
Rebel <rebeliosman@gmail.com>
2020-04-29 20:32:00 +0300

    modified face.rb to file

--- faces/face.rb
+++ faces/face.rb
@@ -5,17 +5,25 @@ if File.exist?("./forehead.txt") ||File.exist?("./eyes.txt")||File.exist?("./nos
   nose = File.new("./nose.txt","r:UTF-8")
   mouth = File.new("./mouth.txt","r:UTF-8")
 
+  time = Time.now
+  file_name = time.strftime("%Y-%m-%d-%H-%M")
+
+  current_path = File.dirname(__FILE__)
+  file = File.new(current_path + "/face" + file_name + ".txt", "a:UTF-8")
+
   line = forehead.readlines
-  puts line[rand(line.size)]
+  file.puts line[rand(line.size)]
 
   line = eyes.readlines
-  puts line[rand(line.size)]
+  file.puts line[rand(line.size)]
 
   line = nose.readlines
-  puts line[rand(line.size)]
+  file.puts line[rand(line.size)]
 
   line = mouth.readlines
-  puts line[rand(line.size)]
+  file.puts line[rand(line.size)]
+
+  file.close
 
 else
   puts "One of parts not found."
