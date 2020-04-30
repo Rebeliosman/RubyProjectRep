abbb4d5f81c9207bd26009c654f6d5bfa4177611
Rebel <rebeliosman@gmail.com>
2020-04-30 14:27:02 +0300

    goverment holliday added to wday.rb

--- Wday.rb
+++ Wday.rb
@@ -1,7 +1,29 @@
 time = Time.now
 week_day = time.wday
-if (week_day==0 && week_day==6)
-  puts "Stay home"
+
+if File.exist?("./hol.txt")
+
+current_path = File.dirname(__FILE__)
+file = File.new(current_path + "/hol.txt")
+holidays_arr = file.readlines
+file.close
+
+  if (week_day==0 && week_day==6)
+    puts "Stay home"
+  end
+
+for day in holidays_arr
+  day.chomp!
+end
+
+date = time.strftime('%d.%m')
+
+  if holidays_arr.include?(date)
+    puts 'Goverment holiday'
+  else
+    puts 'Go to job MF'
+ end
+
 else
-puts "Go to job mf"
+  puts "file not found"
 end
