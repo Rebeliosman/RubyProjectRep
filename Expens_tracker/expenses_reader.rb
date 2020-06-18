8e93c0407acc44a62b5ef4e4f892344fd76528cf
Сурков Иван Григорьевич <SurkovIG@tdhc.ru>
2020-06-18 16:25:33 +0300

    EXPtraker working

--- Expens_tracker/expenses_reader.rb
+++ Expens_tracker/expenses_reader.rb
@@ -1,42 +1,62 @@
-require 'rexml/document'# парсер XML
-require 'date' # библиотека для работы с датами
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
 
-current_path = File.dirname(__FILE__) # путь до директории исп. файла
-file_name = current_path + '/data/expenses.xml'  # файл
-
-file = File.new(file_name)
-
-file_xml = REXML::Document.new (file) # новый файл REXML из файла XLM
-
-amount_by_day = Hash.new # пустой асс. массив
-
-file_xml.elements.each("expenses/expense") do |item|
-loss_sum = item.attributes['amount'].to_i #сумма сколько потратили из параметра amount
-loss_date = Date.parse (item.attributes['date']) #когда потратили из параметра date
-
-amount_by_day[loss_date] ||= 0 # иницилизация нулем значения хэша, если этой даты еще не было
-amount_by_day[loss_date] += loss_sum # + трата в этот день
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
 end
+
+require 'rexml/document'# Подключаем парсер rexml
+require 'date' #и библиотеку date
+
+file_name = File.dirname(__FILE__) + '/data/expenses.xml'
+
+unless File.exist?(file_name)
+  abort "Извиняемся, хозяин, файлик #{file_name} не найден." # Не найден файл, завершаем программу.
+end
+
+file = File.new(file_name) # открытие файла
+
+doc = REXML::Document.new(file)
 
 file.close
 
-#собераем в массив сумму расходов за каждый месяц
-sum_by_month = Hash.new
-amount_by_day.keys.sort.each do |key|
-sum_by_month[key.strftime("%B %Y")] ||= 0  # key.strftime("%B %Y") вернет одинаковую строку для всех дней одного месяца
-sum_by_month[key.strftime("%B %Y")] += amount_by_day[key] # + сумма расходов за следующий день
+amount_by_day = {}
+
+
+doc.elements.each('expenses/expense') do |item|
+
+  loss_sum = item.attributes['amount'].to_i
+
+  loss_date = Date.parse(item.attributes['date'])
+
+  amount_by_day[loss_date] ||= 0
+
+  amount_by_day[loss_date] += loss_sum
 end
 
-# вывод
+sum_by_month = {}
 
-current_month = amount_by_day.keys.sort[0].strftime("%B %Y")
-puts "------[ #{current_month}, всего потрачено: #{sum_by_month[current_month]} р. ]--------"
-
-# цикл по всем дням
 amount_by_day.keys.sort.each do |key|
-if key.strftime("%B %Y") != current_month
-  puts "------[ #{current_month}, всего потрачено: #{sum_by_month[current_month]} р. ]--------"
+
+  sum_by_month[key.strftime('%B %Y')] ||= 0
+
+    sum_by_month[key.strftime('%B %Y')] += amount_by_day[key]
 end
-# расходы за конкретный день
-puts "\t#{key.day}: #{amount_by_day[key]} р."
+
+current_month = amount_by_day.keys.sort[0].strftime('%B %Y')
+
+puts "------[ #{current_month}, всего потрачено: " \
+  "#{sum_by_month[current_month]} р. ]--------"
+amount_by_day.keys.sort.each do |key|
+  if key.strftime('%B %Y') != current_month
+
+    current_month = key.strftime('%B %Y')
+
+    puts "------[ #{current_month}, всего потрачено: " \
+      "#{sum_by_month[current_month]} р. ]--------"
+  end
+
+  puts "\t#{key.day}: #{amount_by_day[key]} р."
 end
